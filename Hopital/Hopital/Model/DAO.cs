a8e9da488683d813f8c8f818f3be02fed07c79a7
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-06-03 13:41:38 +0200

    Add generique DAO

---
+++ Hopital/Hopital/Model/DAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hopital.Model
+{
+    public interface Dao<T, PK>
+    {
+        List<T> FindAll(); //aussi appelée SelectAll
+        T FindById(PK id);
+        void Create(T obj); //Insert
+        void Update(T obj);
+        void Delete(PK id);
+    }
+}
