6233a22803ff1e1f2b2d695065b533e63b874df4
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-06-03 15:33:05 +0200

    Add sqlServer singleton

---
+++ Hopital/Hopital/Model/SqlServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Hopital.Utils;
+namespace Hopital.Model
+{
+    class SqlServer
+    {
+        private static SqlServer This;
+        private SqlServer()
+        {
+            string connectionString = FakeEnvironment.GetEnvVar("SQL_PROPERTIES");
+            Console.WriteLine("connectionString: {0}", connectionString);
+            if (connectionString == null)
+                throw new EnvVarNotDefinedException("SQL_PROPERTIES");
+            Connection = new SqlConnection(connectionString);
+        }
+        public static SqlServer Get()
+        {
+            if (This == null)
+                This = new SqlServer();
+            return This;
+        }
+        public SqlConnection Connection { get; internal set; }
+    }
+}
