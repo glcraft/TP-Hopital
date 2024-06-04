48f095340efc3b593e9f5aa206c3698b9f3562a1
Gabin Lefranc <gabin.lefranc@gmail.com>
2024-06-04 14:02:22 +0200

    Fix Sad Staff

--- Hopital/Hopital/Model/DaoStaffSqlServer.cs
+++ Hopital/Hopital/Model/DaoStaffSqlServer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Data;
@@ -9,8 +9,6 @@ namespace Hopital.Model
 {
     class DaoStaffSqlServer : IDaoStaff
     {
-       // private string connectionString = @"Data Source=ENVY\SQLEXPRESS;Initial Catalog=Hopital-tp1;Integrated Security=True";
-       //SqlConnection connection = new SqlConnection(connectionString);
         SqlConnection connection = SqlServer.Get().Connection;
 
         public void Create(Staff obj)
@@ -18,7 +16,7 @@ namespace Hopital.Model
             throw new NotImplementedException();
         }
 
-        public void Delete(int id)
+        public void Delete(string id)
         {
             throw new NotImplementedException();
         }
@@ -65,7 +63,7 @@ namespace Hopital.Model
             return staffList;
         }
  
-        public Staff FindById(int id)
+        public Staff FindById(string id)
         {
             throw new NotImplementedException();
         }
--- Hopital/Hopital/Model/IDaoStaff.cs
+++ Hopital/Hopital/Model/IDaoStaff.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Hopital.Model
 {
  
-    interface IDaoStaff : IDao<Staff, int>
+    interface IDaoStaff : IDao<Staff, string>
     {
         Staff Login(string login, string password);
     }
